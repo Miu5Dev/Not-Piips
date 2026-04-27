a9d5e38d111c91307d1b76749931868d220e0cce
KumuIi <chripers08@gmail.com>
2026-04-27 10:35:17 +0200

    Ported To new version of Eventsystem

--- Assets/Scripts/InventorySystem/OpenIventory.cs
+++ Assets/Scripts/InventorySystem/OpenIventory.cs
@@ -13,13 +13,10 @@ public class OpenIventory : MonoBehaviour
 
     void Start()
     {
-        EventBus.Subscribe<OnOpenInventoryEvent>(OnOpenInventory);
         gameObject.SetActive(false);
     }
 
-    void OnDestroy() => EventBus.Unsubscribe<OnOpenInventoryEvent>(OnOpenInventory);
-
-    void OnOpenInventory(OnOpenInventoryEvent e)
+    public void OnOpenInventory(OnOpenInventoryEvent e)
     {
         if (e.pressed)
             gameObject.SetActive(!gameObject.activeSelf);
