ee0f04044302f74c3ad9e9eb2f435c214613a69c
awaMius <83723326+Miu5Dev@users.noreply.github.com>
2026-05-11 13:16:44 +0200

    Update

---
+++ Assets/BossBarManager.cs
@@ -0,0 +1,60 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossBarManager : MonoBehaviour
+{
+    
+    public GameObject bossBar;
+    
+    public LayerMask BossLayer;
+    public Slider BossSlider;
+    public TMP_Text BossName;
+    public TMP_Text BossHealth;
+
+    private int lastHealth;
+
+    private BossController controller;
+    private HealthController hp;
+
+    public void onPingEventReceived(OnPingEvent e)
+    {
+        if ((BossLayer.value & (1 << e.sender.layer)) != 0)
+        {
+            controller = e.sender?.GetComponent<BossController>();
+            BossSlider.maxValue = controller._health.maxHealth;
+            BossSlider.minValue = 0; 
+            BossSlider.value = controller._health.maxHealth;
+            BossName.text = controller.bossData.bossName;
+            hp = controller._health;
+            lastHealth = controller._health.maxHealth;
+
+            EventBus.Raise(new OnPingEvent()
+            {
+                sender = this.gameObject
+            });
+        }
+    }
+
+    public void LateUpdate()
+    {
+        
+        if (controller == null || hp == null || hp.isDead)
+        {
+            if(bossBar.active)
+                bossBar.SetActive(false);
+            return;
+        }
+        
+        if(!bossBar.active)bossBar.SetActive(true);
+        
+        if (lastHealth != controller._health.health)
+        {
+            lastHealth = controller._health.health;
+            BossHealth.text = $"{hp.health} / {hp.maxHealth}";
+            BossSlider.value = hp.health;
+        }
+        
+    }
+}
